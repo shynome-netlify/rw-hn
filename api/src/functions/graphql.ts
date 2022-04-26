686c01711718ee27590b8885bb25c00f5e104ec1
shynome <shynome@gmail.com>
2022-04-26 08:59:37 +0800

    remove db deps

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -4,7 +4,7 @@ import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
 import services from 'src/services/**/*.{js,ts}'
 
-import { db } from 'src/lib/db'
+// import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
@@ -14,6 +14,6 @@ export const handler = createGraphQLHandler({
   services,
   onException: () => {
     // Disconnect from your database with an unhandled exception.
-    db.$disconnect()
+    // db.$disconnect()
   },
 })
