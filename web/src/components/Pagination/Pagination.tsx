068a7ad9060eb915705c3742bb0f19e01bde851c
shynome <shynome@gmail.com>
2022-04-27 08:50:07 +0800

    impl pagiation

--- web/src/components/Pagination/Pagination.tsx
+++ web/src/components/Pagination/Pagination.tsx
@@ -9,6 +9,39 @@ type PaginationProps = {
   display_pages?: number
 }
 
+function getPages(now: number, end_page: number, cap: number): number[] {
+  let maybeList = []
+  for (let i = now - cap; i < now + cap; i++) {
+    maybeList.push(i)
+  }
+  maybeList = maybeList.filter((n) => n >= 0 && n <= end_page)
+
+  const nowIndex = maybeList.indexOf(now)
+  if (nowIndex === 0) {
+    return maybeList.slice(0, cap)
+  }
+  if (maybeList.length <= cap) {
+    return maybeList
+  }
+
+  const lastIndex = maybeList.length - 1
+  let [a, b] = [nowIndex, nowIndex]
+  const arr = [nowIndex]
+  while (arr.length < cap) {
+    a = a - 1
+    b = b + 1
+    if (a >= 0) {
+      arr.unshift(a)
+    }
+    if (b <= lastIndex) {
+      arr.push(b)
+    }
+  }
+  const result = arr.map((i) => maybeList[i])
+
+  return result
+}
+
 const Pagination: React.FC<PaginationProps> = ({
   now,
   count,
@@ -16,8 +49,9 @@ const Pagination: React.FC<PaginationProps> = ({
   per_page = 15,
   display_pages = 5,
 }) => {
-  const end_page = Math.ceil(count / per_page)
-  const pages = [0, 1, 2, 3, 4]
+  const end_page = Math.floor(count / per_page)
+  const pages = getPages(now, end_page, display_pages)
+
   return (
     <div className={`flex space-x-4 ${styles.root}`}>
       <Link to={gen(0)}>{'<<'}</Link>
